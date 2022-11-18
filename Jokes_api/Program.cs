26c51e9991de5bf8ffe1f17be656b9d90ca1562c
Carl-Anders Arosenius <arosenius.carl-anders@elev.ga.ntig.se>
2022-11-18 09:58:13 +0100

    first commit

---
+++ Jokes_api/Program.cs
@@ -0,0 +1,8 @@
+﻿using RestSharp;
+using System.Text.Json;
+using System.Net;
+RestClient jokeClient = new ("https://api.jokes.one");
+
+RestRequest request = new("pokemon/rayquaza");
+
+RestResponse response = jokeClient.GetAsync(request).Result;
